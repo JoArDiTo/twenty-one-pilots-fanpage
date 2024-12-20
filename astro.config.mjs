169d047b36ae444b076d431c57f5be8d0fab3cbf
JoArDiTo <sm.armando05@gmail.com>
2024-12-20 16:38:55 -0500

    add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+
   output: 'hybrid',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel()
 });
